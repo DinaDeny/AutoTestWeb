b7f5c22ae71187adc0e8081ce7c3f127e87dd32c
DinaDeny <denisenkodina20@gmail.com>
2020-05-07 14:02:11 +0300

    some updates with tests add testng concepts

--- src/test/java/AutoTestWeb/AppTest.java
+++ src/test/java/AutoTestWeb/AppTest.java
@@ -1,61 +1,76 @@
 package AutoTestWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.w3c.dom.html.HTMLSelectElement;
+import org.testng.annotations.BeforeClass;
 
-import javax.swing.*;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class AppTest {
 
     private WebDriver driver;
 
-    @Test
-    public void myTest(){
+    @BeforeClass
+    public void innitDriver(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("https://www.ranorex.com/web-testing-examples/vip/");
-
+    }
+    @Test()
+    public void myTest() {
         WebElement firstNameField = driver.findElement(By.id("FirstName"));
         firstNameField.click();
         firstNameField.sendKeys("Jenifer");
         firstNameField.clear();
         firstNameField.sendKeys("VeryLongTestFirstNameWithAnyCharacters1!@#$%^&*");
         firstNameField.clear();
-
+    }
+     @Test
+     public void myTest1(){
         WebElement lastNameField = driver.findElement(By.id("LastName"));
         lastNameField.click();
         lastNameField.sendKeys("Aniston");
-        firstNameField.clear();
+        lastNameField.clear();
         lastNameField.sendKeys("VeryLongTestLastNameWithAnyCharacters1!@#$%^&*");
         lastNameField.clear();
-
-        WebElement Category = driver.findElement(By.id("Category"));
-        Select select = new Select(Category);
-        select.getAllSelectedOptions();
-        select.getFirstSelectedOption();
-        select.selectByValue("Science");
-
-        WebElement SaveButton = driver.findElement(By.id("Save"));
-        Actions action = new Actions (driver);
-        action.doubleClick(SaveButton);
-
-
         driver.navigate().refresh();
-        driver.quit();
+    }
+        @Test
+        public void myTest2() {
 
+            WebElement Category = driver.findElement(By.id("Category"));
+            Select select = new Select(Category);
+            select.getAllSelectedOptions();
+            select.getFirstSelectedOption();
+            select.selectByValue("Science");
+
+        }
+
+         @Test
+         public void myTest3(){
+         WebElement SaveButton = driver.findElement(By.id("Save"));
+         Actions action = new Actions(driver);
+         action.doubleClick(SaveButton);
+        }
+
+
+
+
+
+           @AfterClass
+               public void tearDown() {
+           driver.quit();
+       }
     }
 
 
-}
+
+
